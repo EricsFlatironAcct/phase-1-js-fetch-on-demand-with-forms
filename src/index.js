fd5ead350313a475b89e75a022483d4355ac96cb
Eric Krukowski <erickruk@amazon.com>
2023-03-20 05:04:23 -0400

    completed test handling 404 error

--- src/index.js
+++ src/index.js
@@ -4,15 +4,20 @@ const init = () => {
   inputForm.addEventListener("submit", (event)=>{
     event.preventDefault();
     const input = document.querySelector("input#searchByID");
-    console.log(input.value);
-
     fetch(`http://localhost:3000/movies/${input.value}`)
-    .then(resp=>resp.json())
-    .then(data =>{
-        const title = document.querySelector("section#movieDetails h4");
-        title.innerText = data.title;
-        const summary = document.querySelector("section#movieDetails p");
-        summary.innerText = data.summary;
+    .then((resp)=>{
+        if(resp.status === 404){
+            document.querySelector("section#movieDetails p").innerText = "";
+            document.querySelector("section#movieDetails h4").innerText = "Unable to find movie";
+            return;
+        }
+        resp.json()
+        .then(data =>{
+            const title = document.querySelector("section#movieDetails h4");
+            title.innerText = data.title;
+            const summary = document.querySelector("section#movieDetails p");
+            summary.innerText = data.summary;
+        })  
     })
   });
 }
